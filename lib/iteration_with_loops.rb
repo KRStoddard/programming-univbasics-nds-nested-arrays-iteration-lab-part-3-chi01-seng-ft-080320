d4982f70e2525995ae17a4169eee288f8e0dbe1c
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-02 17:19:35 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
     element_count = 0 
     while element_count < src[index_count].length 
     if src[index_count][element_count].is_a? String
-      newarray.push(src[index_count])
+      newarray.push(src[index_count][element_count])
     end
     element_count += 1 
   end
