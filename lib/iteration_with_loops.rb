bd34248e6f3212a17969bfd6cd732ea39b663d6d
Kimberlyn Stoddard <kimberlynrstoddard@gmail.com>
2020-07-02 17:08:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,4 +1,15 @@
 def join_nested_strings(src)
   index_count = 0 
-  newstring = 
+  newarray = []
+  while index_count < src.length do
+    element_count = 0 
+    while element_count < src[index_count].length 
+    if src[index_count] == "#{src[index_count}"
+      newarray.push(src[index_count])
+    end
+    element_count += 1 
+  end
+  index_count += 1 
+end
+newarray.split("")
 end
